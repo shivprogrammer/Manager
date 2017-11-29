10d08957768aa95ebb09794d3505d9e399baa51e
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-28 17:28:52 -0800

    added firebase as dep and updated App.js for firebase incorporation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCteX76S5ztZieAHq9Lx1SivIXlo-yG-N8',
+      authDomain: 'shivvymanager.firebaseapp.com',
+      databaseURL: 'https://shivvymanager.firebaseio.com',
+      projectId: 'shivvymanager',
+      storageBucket: 'shivvymanager.appspot.com',
+      messagingSenderId: '1016351005250'
+    };
+
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
