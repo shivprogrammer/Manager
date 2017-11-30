20513e6c2fc0010d05f16fb15e1b23387d8c5d8f
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-29 18:18:47 -0800

    employee create scene is functioning and appearing quite nicely [iOS WORKING]

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -2,10 +2,18 @@ import {
   EMPLOYEE_UPDATE
 } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { prop: 'name', value: 'jane' }
+      return { ...state, [action.payload.prop]: action.payload.value };
+      // this is NOT an array, this is key interpolation, so adding another key
     default:
     return state;
   }
