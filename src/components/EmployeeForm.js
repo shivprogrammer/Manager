99cc6490550986dd2ec4b02780c031e7f960d1f2
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-30 17:22:05 -0800

    started EmployeeEdit

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Picker } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { CardSection, Input } from './common';
 
 class EmployeeForm extends Component {
@@ -53,4 +55,10 @@ const styles = {
   }
 };
 
-export default EmployeeForm;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeForm);
