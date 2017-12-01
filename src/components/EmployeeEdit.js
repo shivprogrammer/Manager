490ec88fc48396dc5cf9f301f374e407d1940062
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-30 17:27:00 -0800

    more set up for EmployeeEdit and hooking it up to router

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import EmployeeForm from './EmployeeForm';
 import { Card, CardSection, Button } from './common';
 
 class EmployeeEdit extends Component {
   render() {
     return (
       <Card>
+        <EmployeeForm />
         <CardSection>
           <Button>
             Save Changes
@@ -14,3 +17,5 @@ class EmployeeEdit extends Component {
     );
   }
 }
+
+export default connect()(EmployeeEdit);
